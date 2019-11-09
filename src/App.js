2361473a4a60cfb0b1e430078c6f94fd11b81ba9
Jonas Johan Solsvik <jonasjso@pm.me>
2019-11-09 20:01:41 +0100

    create Map react component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Map from './components/Map';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Mapbox PWA demo
         </p>
       </header>
+      <Map />
     </div>
   );
 }
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,9 @@
-import {Component} from 'react';
+import React, {Component} from 'react';
 
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+
+import './Map.css';
 
 class Map extends Component {
 
@@ -13,14 +15,27 @@ class Map extends Component {
         }
     }
 
+    compnentDidMount() {
+        mapboxgl.accessToken = 'undefined';
+        
+        const map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v10',
+            center: [0, 0],
+            zoom: 9,
+        });
+
+        map.on('load', () => {});
+
+        this.setState({
+            map,
+        });
+    }
 
     render() {
-        return {
-            <div id="mapContainer">
-            
-            
-            </div>
-        }
+        return (
+            <section id="map" />
+        );
     }
 }
 
