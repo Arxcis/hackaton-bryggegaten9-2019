50353ab24266f38f8db0ce35f40bdc12ff828ae1
Jonas Johan Solsvik <jonasjso@pm.me>
2019-11-10 20:02:35 +0100

    route.json - add route
    - use arc to account for incorrect mercator projection for straight lines

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,6 +4,9 @@ import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 import './Map.css';
+import { segments } from './route.json';
+import arc from 'arc';
+
 
 class Map extends Component {
 
@@ -13,6 +16,7 @@ class Map extends Component {
         this.state = {
             map: null,
             online: false,
+            geo: [],
         }
     }
 
@@ -32,15 +36,18 @@ class Map extends Component {
             style: 'mapbox://styles/mapbox/streets-v10',
             ...JSON.parse(state),
         });
-        map.on('load', () => {});
+        map.on('load', () => {
+            const lineLayer = makeLineLayer();
+            map.addLayer(lineLayer);
+        });
 
         this.setState({ map });
+        this.setState({ online: window.navigator.onLine });
 
         window.addEventListener('online', this.onOnline)
         window.addEventListener('offline', this.onOffline)
-        this.setState({ online: window.navigator.onLine });
-
         window.addEventListener('beforeunload', this.storeMapState)
+        
     }
 
     componentWillUnmount() {
@@ -57,9 +64,12 @@ class Map extends Component {
             zoom: this.state.map.getZoom(),
         };
         window.localStorage.setItem('map-options', JSON.stringify(state));
+        
+        return;
     }
 
     render() {
+
         const { online } = this.state;
         let offlineMessage;
         if (!online) {
@@ -71,6 +81,61 @@ class Map extends Component {
                 <section id="map" />
             </section>
         );
+    }
+}
+
+
+function makeLineLayer() {
+    //
+    // Parse route
+    //
+    const arcs = [];
+    segments.map((it) => {
+        var arcResolution = Math.ceil(it.distance/10)
+        var generator = new arc.GreatCircle({x: it.from.lng, y:it.from.lat}, {x: it.to.lng, y:it.to.lat}, {})
+        var geojson = generator.Arc(arcResolution)
+    
+        arcs.push(geojson.json())
+    });
+
+
+
+    const coordinates = arcs.reduce((normalizedCoordinates, item) => { 
+        return [
+            ...normalizedCoordinates,
+            ...item.geometry.coordinates.slice(0, item.geometry.coordinates.length-1),
+        ]
+    }, []);
+
+/*     const coordinates = segments.reduce((coordinates, { from: { lat, lng } }) => {
+        return [ ...coordinates, [lng, lat] ]; 
+    }, []);
+
+    console.log(coordinates)
+ */
+
+    return {
+        "id": "route",
+        "type": "line",
+        "source": {
+            "type": "geojson",
+            "data": {
+                "type": "Feature",
+                "properties": {},
+                "geometry": {
+                    "type": "LineString",
+                    coordinates,
+                }
+            }
+        },
+        "layout": {
+            "line-join": "round",
+            "line-cap": "round"
+        },
+        "paint": {
+            "line-color": "#888",
+            "line-width": 8
+        }
     }
 }
 
